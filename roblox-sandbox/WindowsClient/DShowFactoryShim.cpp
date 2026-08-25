630fa9268dcab8360880a032e855e832e1ff2e61
whereverthewindblows <262692623+bartholomort@users.noreply.github.com>
2026-08-25 13:13:41 -0400

    build: include stdafx pch in dshow factory shim

--- roblox-sandbox/WindowsClient/DShowFactoryShim.cpp
+++ roblox-sandbox/WindowsClient/DShowFactoryShim.cpp
@@ -1,6 +1,7 @@
 // DirectShow base-class contract: strmbase.lib's dllentry.obj references the
 // consumer's class-factory registry. WindowsClient implements no COM server;
 // supply the empty table the library requires.
+#include "stdafx.h"
 #include <streams.h>
 
 CFactoryTemplate g_Templates[1];
